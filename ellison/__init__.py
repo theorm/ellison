2b7fb24c600e65285e1ac354c06b411c4962f4c3
theorm <theorm@gmail.com>
2012-07-02 15:55:28 +1000

    - updated setup.py

--- ellison/__init__.py
+++ ellison/__init__.py
@@ -1,1 +1,6 @@
 from ellison.base import *
+
+__title__ = 'ellison'
+__version__ = '0.1'
+__author__ = 'Roman Kalyakin'
+__copyright__ = 'Copyright 2012 %s' % __author__
--- setup.py
+++ setup.py
@@ -1,21 +1,28 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+import ellison
 
 setup(
-    name='ellison',
-    version='0.1',
-    url='http://github.com/theorm/ellison/',
-    license='Apache License, Version 2.0',
-    author="Roman Kalyakin",
-    author_email="roman@kalyakin.com",
+    name=ellison.__title__,
+    version=ellison.__version__,
     description="Object oriented mapping for Python and Mongo DB.",
     long_description=open("README.md").read(),
-    keywords=["mongo", "mongodb", "pymongo", "oop", "mapping"],
+    author="Roman Kalyakin",
+    author_email="roman@kalyakin.com",
+    url='http://github.com/theorm/ellison/',
+    license='Apache License, Version 2.0',
+    keywords="mongo mongodb pymongo oop mapping",
+    packages=['ellison'],
+    package_data={'': ['LICENSE', 'README.md']},
+    include_package_data=True,
     install_requires=['pymongo'],
     tests_require=["nose"],
     test_suite="tests",
-    py_modules=['ellison'],
-    classifiers=[
+    classifiers=(
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
@@ -23,5 +30,5 @@ setup(
         'Programming Language :: Python',
         'Topic :: Database',
         'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
+    )
 )
